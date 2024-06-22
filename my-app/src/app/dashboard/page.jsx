34afbd81e32ed5627f5848b033adec3588256fd8
judilegend <judilegend2@gmail.com>
2024-06-22 10:07:21 +0300

    page/operation

--- my-app/src/app/dashboard/page.jsx
+++ my-app/src/app/dashboard/page.jsx
@@ -5,22 +5,13 @@ import Card from "../../components/Card";
 
 const page = () => {
   return (
-    <div className="grid grid-cols-[300px,1fr] grid-rows-[70px,1fr]  h-screen">
-      <div className="row-span-2 ">
-        <Sidebar />
-      </div>
-      <div className="col-span-1 ">
-        {" "}
-        <Navbar />
-      </div>
-      <div className="col-start-2 row-start-2 px-8">
-        <section className="flex w-full justify-between">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </section>
-      </div>
+    <div className="w-full h-screen">
+      <section className="flex w-full justify-between">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </section>
     </div>
   );
 };
