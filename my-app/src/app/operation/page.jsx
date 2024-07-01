a1ab835f5ac70e69658e9bae5ca0bd6c0dd34b84
judilegend <judilegend2@gmail.com>
2024-07-02 01:10:21 +0300

    fix/front-end

--- my-app/src/app/operation/page.jsx
+++ my-app/src/app/operation/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import Operations from "../../components/Operation";
 import Sidebar from "../../components/Sidebar";
@@ -7,57 +7,58 @@ import Navbar from "@/components/Navbar";
 import axios from "axios";
 
 const Page = () => {
-    const [clients, setClients] = useState([]);
-    const [error, setError] = useState(null);
-    const [selectedClient, setSelectedClient] = useState(null);  // State to hold the client to edit
+  const [clients, setClients] = useState([]);
+  const [error, setError] = useState(null);
+  const [selectedClient, setSelectedClient] = useState(null); // State to hold the client to edit
 
-    const fetchClients = async () => {
-        try {
-            const response = await axios.get("http://localhost:8080/client/getAll");
-            setClients(response.data);
-        } catch (error) {
-            setError("Erreur lors de la récupération des données");
-        }
-    };
+  const fetchClients = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/client/getAll");
+      setClients(response.data);
+    } catch (error) {
+      setError("Erreur lors de la récupération des données");
+    }
+  };
 
-    useEffect(() => {
-        fetchClients();
-    }, []);
+  useEffect(() => {
+    fetchClients();
+  }, []);
 
-    const handleDeleteClient = async (clientId) => {
-        try {
-            await axios.delete(`http://localhost:8080/client/delete/${clientId}`);
-            fetchClients(); // Refresh the client list after deletion
-        } catch (error) {
-            console.log("Erreur lors de la suppression du client", error);
-            setError("Erreur lors de la suppression du client");
-        }
-    };
+  const handleDeleteClient = async (clientId) => {
+    try {
+      await axios.delete(`http://localhost:8080/client/delete/${clientId}`);
+      //   console.log(clientId);
+      fetchClients(); // Refresh the client list after deletion
+    } catch (error) {
+      console.log("Erreur lors de la suppression du client", error);
+      setError("Erreur lors de la suppression du client");
+    }
+  };
 
-    return (
-        <div className="w-full grid grid-cols-[300px,1fr] grid-rows-[70px,1fr] h-screen overflow-x-hidden relative">
-            <div className="row-span-2">
-                <Sidebar />
-            </div>
-            <div className="col-span-1">
-                <Navbar />
-            </div>
-            <div className="col-start-2 row-start-2 px-8">
-                <section className="flex w-full justify-between relative">
-                    <div className="mt-10 w-full flex justify-between gap-5">
-                        <Operations
-                            fetchClients={fetchClients}
-                            clients={clients}
-                            error={error}
-                            setSelectedClient={setSelectedClient}
-                            handleDeleteClient={handleDeleteClient} // Pass delete function to Operations
-                        />
-                        <Form client={selectedClient} onFormSubmit={fetchClients} />
-                    </div>
-                </section>
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full grid grid-cols-[300px,1fr] grid-rows-[70px,1fr] h-screen overflow-x-hidden relative">
+      <div className="row-span-2">
+        <Sidebar />
+      </div>
+      <div className="col-span-1">
+        <Navbar />
+      </div>
+      <div className="col-start-2 row-start-2 px-8">
+        <section className="flex w-full justify-between relative">
+          <div className="mt-10 w-full flex justify-between gap-5">
+            <Operations
+              fetchClients={fetchClients}
+              clients={clients}
+              error={error}
+              setSelectedClient={setSelectedClient}
+              handleDeleteClient={handleDeleteClient} // Pass delete function to Operations
+            />
+            <Form client={selectedClient} onFormSubmit={fetchClients} />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
--- my-app/src/components/Form.jsx
+++ my-app/src/components/Form.jsx
@@ -1,122 +1,139 @@
 "use client";
 import React from "react";
 import "../style/Form.css";
-import Axios from "axios"
-import {useForm} from "react-hook-form";
+import Axios from "axios";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 import axios from "axios";
 
+export default function Form({ onFormSubmit, client }) {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({ defaultValues: client });
 
-export default function Form() {
-    const {
-        register, handleSubmit, watch, formState: {errors},
-    } = useForm();
-    const onSubmit = async (data ,e) => {
-        e.preventDefault();
-        try {
-                await axios
-                    .post("http://localhost:8080/client/save", data)
-                    .then(function (response) {
-                        if (response.data) {
-                            alert("Insérer avec succès");
-                        } else {
-                            alert("Erreur d'insertion ...");
-                        }
-                    });
+  useEffect(() => {
+    reset(client);
+  }, [client, reset]);
 
-        } catch (error) {
-            console.log("Erreur d'enregistrement...", error);
-        }    };
+  const onSubmit = async (data, e) => {
+    e.preventDefault();
+    try {
+      const response = client
+        ? await axios.put(
+            `http://localhost:8080/client/update/${client.id_client}`,
+            data
+          )
+        : await axios.post("http://localhost:8080/client/save", data);
 
-    return (<div className="w-[400px]">
-            <div class="form-container">
-                <div class="logo-container">Formlaire Client</div>
+      if (response.data) {
+        alert(client ? "Mise à jour réussie" : "Insertion réussie");
+        onFormSubmit(); // Refresh client list
+      } else {
+        alert(client ? "Erreur de mise à jour" : "Erreur d'insertion");
+      }
+    } catch (error) {
+      console.log(
+        client ? "Erreur de mise à jour" : "Erreur d'enregistrement",
+        error
+      );
+      alert(client ? "Erreur de mise à jour" : "Erreur d'enregistrement");
+    }
+  };
+  return (
+    <div className="w-[400px]">
+      <div class="form-container">
+        <div class="logo-container">Formlaire Client</div>
 
-                <form class="form" onSubmit={handleSubmit(onSubmit)}>
-                    <div className="mt-5">
-                        <label
-                            className="font-semibold text-sm text-gray-600 pb-1 block"
-                            htmlFor="tel"
-                        >
-                            Numero telephone
-                        </label>
-                        <input
-                            className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                            type="text"
-                            id="tel"
-                            {...register("tel")}
-                        />
-                        <label
-                            className="font-semibold text-sm text-gray-600 pb-1 block"
-                            htmlFor="nom"
-                        >
-                            Nom
-                        </label>
-                        <input
-                            className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                            type="text"
-                            id="nom"
-                            {...register("nom")}
-                        />
-                        <label
-                            className="font-semibold text-sm text-gray-600 pb-1 block"
-                            htmlFor="sexe"
-                        >
-                            Sexe
-                        </label>
-                        <select
-                            name="sexe"
-                            id="sexe"
-                            className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                            {...register("sexe")}
-                        >
-                            <option value="Homme">Homme</option>
-                            <option value="Femme">Femme</option>
-                        </select>
-                        <label
-                            className="font-semibold text-sm text-gray-600 pb-1 block"
-                            htmlFor="age"
-                        >
-                            Age
-                        </label>
-                        <input
-                            className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                            type="number"
-                            min={0}
-                            id="age"
-                            {...register("age")}
-                        />
-                        <label
-                            className="font-semibold text-sm text-gray-600 pb-1 block"
-                            htmlFor="solde"
-                        >
-                            Solde
-                        </label>
-                        <input
-                            className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                            type="number"
-                            min={0}
-                            id="age"
-                            {...register("solde")}
-                        />
-                        <label
-                            className="font-semibold text-sm text-gray-600 pb-1 block"
-                            type="email"
-                            htmlFor="email"
-                        >
-                            Email{" "}
-                        </label>
-                        <input
-                            className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-                            type="text"
-                            id="email"
-                            {...register("email")}
-                        />
-                    </div>
+        <form class="form" onSubmit={handleSubmit(onSubmit)}>
+          <div className="mt-5">
+            <label
+              className="font-semibold text-sm text-gray-600 pb-1 block"
+              htmlFor="tel"
+            >
+              Numero telephone
+            </label>
+            <input
+              className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+              type="text"
+              id="tel"
+              {...register("tel")}
+            />
+            <label
+              className="font-semibold text-sm text-gray-600 pb-1 block"
+              htmlFor="nom"
+            >
+              Nom
+            </label>
+            <input
+              className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+              type="text"
+              id="nom"
+              {...register("nom")}
+            />
+            <label
+              className="font-semibold text-sm text-gray-600 pb-1 block"
+              htmlFor="sexe"
+            >
+              Sexe
+            </label>
+            <select
+              name="sexe"
+              id="sexe"
+              className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+              {...register("sexe")}
+            >
+              <option value="Homme">Homme</option>
+              <option value="Femme">Femme</option>
+            </select>
+            <label
+              className="font-semibold text-sm text-gray-600 pb-1 block"
+              htmlFor="age"
+            >
+              Age
+            </label>
+            <input
+              className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+              type="number"
+              min={0}
+              id="age"
+              {...register("age")}
+            />
+            <label
+              className="font-semibold text-sm text-gray-600 pb-1 block"
+              htmlFor="solde"
+            >
+              Solde
+            </label>
+            <input
+              className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+              type="number"
+              min={0}
+              id="age"
+              {...register("solde")}
+            />
+            <label
+              className="font-semibold text-sm text-gray-600 pb-1 block"
+              type="email"
+              htmlFor="email"
+            >
+              Email{" "}
+            </label>
+            <input
+              className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+              type="text"
+              id="email"
+              {...register("email")}
+            />
+          </div>
 
-                    <button class="form-submit-btn" type="submit">
-                        Enregistrer
-                    </button>
-                </form>
-            </div>
-        </div>);
+          <button class="form-submit-btn" type="submit">
+            {client ? "Mettre à jour" : "Enregistrer"}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
