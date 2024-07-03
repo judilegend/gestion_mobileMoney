cd74e218347d59077ba99b29f7c4a1a059a280c7
judilegend <judilegend2@gmail.com>
2024-07-03 23:35:05 +0300

    manage-transfert

---
+++ src/app/client/data.js
@@ -0,0 +1,15 @@
+import React from "react";
+export const Months = [
+  { key: "janvier", label: "Janvier" },
+  { key: "fervrier", label: "Fervrier" },
+  { key: "mars", label: "Mars" },
+  { key: "avril", label: "Avril" },
+  { key: "Mai", label: "Mai" },
+  { key: "Juin", label: "Juin" },
+  { key: "Juillet", label: "Juillet" },
+  { key: "Aout", label: "Aout" },
+  { key: "Septembre", label: "Septembre" },
+  { key: "Octobre", label: "Octobre" },
+  { key: "Novembre", label: "Novembre" },
+  { key: "Decembre", label: "Decembre" },
+];
