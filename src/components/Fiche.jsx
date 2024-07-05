89490f8c6b3a2f12095f10c8245dfa8d95184e25
judilegend <judilegend2@gmail.com>
2024-07-05 14:54:40 +0300

    add/component-dash

---
+++ src/components/Fiche.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@nextui-org/react";
+
+const Fiche = ({ clients, error, setSelectedClient, handleDeleteClient }) => {
+  return (
+    <section className="w-[1100px]">
+      {error && <p className="text-red-500">{error}</p>}
+      <Table aria-label="Tableau de clients">
+        <TableHeader>
+          <TableColumn>TEL</TableColumn>
+          <TableColumn>NOM</TableColumn>
+          <TableColumn>SEXE</TableColumn>
+          <TableColumn>AGE</TableColumn>
+          <TableColumn>SOLDE</TableColumn>
+          <TableColumn>EMAIL</TableColumn>
+          <TableColumn>ACTIONS</TableColumn>
+        </TableHeader>
+        <TableBody>
+          {clients.map((client) => (
+            <TableRow key={client._id}>
+              <TableCell>{client.numTel}</TableCell>
+              <TableCell>{client.nom}</TableCell>
+              <TableCell>{client.sexe}</TableCell>
+              <TableCell>{client.age}</TableCell>
+              <TableCell>{client.solde}</TableCell>
+              <TableCell>{client.mail}</TableCell>
+              <TableCell>
+                <button onClick={() => setSelectedClient(client)}>Edit</button>
+                <button onClick={() => handleDeleteClient(client._id)}>
+                  Delete
+                </button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </section>
+  );
+};
+
+export default Fiche;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <main className="sidebar h-screen fixed  top-0 bottom-0 lg:left-0 left-[-300px] p-2 w-[300px] overflow-y-auto text-center bg-gray-900">
+      {/* <section className=" mx-auto w-fit ">
+        <div className="text-lg font-bold text-gray-600 ">
+          G-Mobile <span className="text-bleu">Money</span>
+        </div>
+      </section> */}
+      <div class="text-gray-100 text-xl">
+        <div class="p-2.5 mt-1 flex items-center">
+          <i class="bi bi-app-indicator px-2 py-1 rounded-md bg-blue-600"></i>
+          <h1 class="font-bold text-gray-200 text-[15px] ml-3">
+            G-Mobile Money
+          </h1>
+          <i
+            class="bi bi-x cursor-pointer ml-28 lg:hidden"
+            onclick="Close()"
+          ></i>
+        </div>
+        <div class="my-2 bg-gray-600 h-[1px]"></div>
+      </div>
+      <section className="">
+        <ul>
+          <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+            {/* <i className="bi bi-house-door-fill"></i> */}
+            <img src="/icons.png" alt="" />
+            <span className="text-[15px] ml-4 text-gray-200 font-bold">
+              <a href="dashboard"> Tableau de board</a>
+            </span>
+          </div>
+          <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+            <img src="/icon/operation.svg" alt="" />
+            <span className="text-[15px] ml-4 text-gray-200 font-bold">
+              <a href="client">Client</a>
+            </span>
+          </div>
+          <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+            <img
+              src="/icon/Initiate Money Transfer.svg"
+              alt=""
+              className="w-6"
+            />
+            <span className="text-[15px] ml-4 text-gray-200 font-bold">
+              <a href="envoyer">Envoyer</a>
+            </span>
+          </div>
+          <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+            <img src="/icon/Receive Dollar.svg" alt="" className="w-6" />
+            <span className="text-[15px] ml-4 text-gray-200 font-bold">
+              <a href="retrait">Retirer</a>
+            </span>
+          </div>
+          <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+            <img src="/icon/Unread messages.svg" alt="" className="w-6" />
+            <span className="text-[15px] ml-4 text-gray-200 font-bold">
+              Notification
+            </span>
+          </div>
+        </ul>
+      </section>
+    </main>
+  );
+};
+
+export default Sidebar;
